8be84a6822e7fd25dc0a2d6750e6b40b56d892f9
yair marin <edgar.marin@axity.com>
2023-06-13 12:15:00 -0600

    intro redux

---
+++ toolkit-redux/src/App.jsx
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux'
+import './App.css'
+import { decrement, increment, incrementBy } from './store/slices/counter';
+
+function App() {
+
+  const { counter } = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <h1> Count is {counter} </h1>
+      <div className="card">
+        <button onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch(incrementBy(2))}>
+          Increment by 2
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
