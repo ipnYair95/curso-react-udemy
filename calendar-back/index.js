a16c3ad857b663fdc35f102af4bdada9c34d36a0
Yair Marin <edgar.marin@axity.com>
2025-02-28 15:25:04 -0600

    curso react

---
+++ calendar-back/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {    
+
+    res.json({
+        ok: true
+    });
+
+})
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
